ddc9c62ddd6b0de51c6850627567afad73a9a25e
Sanjay <sanjaysalunkhe14@gmail.com>
2021-02-20 18:02:13 +0530

    added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'tasks#index'
+
+  resources :users do 
+  	resources :tasks do 
+  	end
+  end
 end
